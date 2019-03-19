ba658e79186fddb2093c948c0534c037e86633ab
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-03-19 17:40:02 -0400

    Allow zooming room view with ctrl+mouse wheel

--- mcedit_ui/room_view.py
+++ mcedit_ui/room_view.py
@@ -4,10 +4,23 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 
 class RoomView(QGraphicsView):
+  ZOOM_SCALES = [
+    0.25,
+    0.5,
+    0.75,
+    1.0,
+    1.5,
+    2.0,
+    2.5,
+    3.0,
+    4.0,
+  ]
+  
   def __init__(self, parent):
     super().__init__(parent)
     
     self.is_panning = False
+    self.curr_zoom_index = self.ZOOM_SCALES.index(1.0)
     
     self.setMouseTracking(True)
   
@@ -63,3 +76,24 @@ class RoomView(QGraphicsView):
       return
     
     super().mouseReleaseEvent(event)
+  
+  def wheelEvent(self, event):
+    if not QApplication.keyboardModifiers() & Qt.ControlModifier:
+      return
+    
+    y_change = event.angleDelta().y()
+    
+    if y_change > 0:
+      if self.curr_zoom_index == len(self.ZOOM_SCALES) - 1:
+        return
+      self.curr_zoom_index += 1
+    elif y_change < 0:
+      if self.curr_zoom_index == 0:
+        return
+      self.curr_zoom_index -= 1
+    else:
+      return
+    
+    curr_zoom_scale = self.ZOOM_SCALES[self.curr_zoom_index]
+    self.resetTransform()
+    self.scale(curr_zoom_scale, curr_zoom_scale)
